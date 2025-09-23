7a6c271c105a19714b7eb583ffe698736093642a
taoken255 <taoken255@gmail.com>
2025-09-23 16:42:25 +0300

    fix sleep

--- run.sh
+++ run.sh
@@ -18,4 +18,4 @@ echo "Starting Render"
 python3 /app/server.py &
 
 echo "Starting WebRTC service"
-uv run simple_webrtc_server.py --host 0.0.0.0 --port 8080
+sleep 30 && uv run simple_webrtc_server.py --host 0.0.0.0 --port 8080
